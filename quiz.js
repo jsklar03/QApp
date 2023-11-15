915efe1f840459dd2954aaf07a3ccedbe7af0b65
jsklar03 <jonah.sklar@gmail.com>
2023-11-15 17:52:24 -0500

    new questions in qbank

--- quiz.js
+++ quiz.js
@@ -11,21 +11,18 @@ console.log(topic);
 const level = params.get('level');
 console.log(level)
 
-//defines the inputs to the quizzle class object. NEED TO UPDATE THESE TO POPULATE DYNAMICALLY
+//defines the inputs to the quizzle class object. 
+// sets quiz source, quiz level, quiz mc options, quiz answers
 let quiz_source = quiz_bank["topic"][topic]
 let quiz_level = quiz_bank["topic"][topic]["levels"][level]
 let quiz_mc = quiz_bank["topic"][topic]["levels"][level + "_mc"]
-console.log(quiz_level)
-console.log(quiz_mc)
+let quiz_answers = quiz_bank["topic"][topic]["levels"][level + "_a"]
 
-console.log(localStorage.getItem('storedItem'))
+// console.log(localStorage.getItem('storedItem'))
 
 //kicks off the createQuizzes function
 function populate_quiz(){
-    const topic_listener = document.querySelector('.quiz_list');
-    console.log(topic_listener);
     console.log("first function triggered")
-    
     updateQuizCard();
 }
 
@@ -33,28 +30,12 @@ function populate_quiz(){
 let card= document.querySelector('.card_placeholder');
 let question_number= document.querySelector('.question_number');
 let card_question = document.querySelector('.question');
-// let answer_list= document.querySelector('answer_list');
-let option_1= document.querySelector('#a');
-let option_2= document.querySelector('#b');
-let option_3= document.querySelector('#c');
-let option_4= document.querySelector('#d');
 let back_btn= document.querySelector('.back');
 let forward_btn= document.querySelector('.forward');
 
 let q_counter = 0
-//gets the answer key
-let quiz_answers = quiz_bank["topic"][topic]["levels"][level + "_a"]
-console.log(quiz_answers)
-// let a1 = quiz_answers[0]
-// console.log(a1);
-// let b1 = quiz_answers[1]
-// let c1 = quiz_answers[2]
-// let d1 = quiz_answers[3]
 
 let question_array = [quiz_level[0],quiz_level[1],quiz_level[2],quiz_level[3],quiz_level[4]]
-// let answer_array = [a1,b1,c1,d1]
-
-// console.log(question_array[0])
 
 function updateQuizCard(){
     console.log(q_counter);
@@ -91,7 +72,6 @@ function goBackQuestion(){
 }
 
 let mc_counter = 0;
-console.log(quiz_mc[0])
 
 function updateMultipleChoice(){
     console.log("update MC triggered");
@@ -102,4 +82,19 @@ function updateMultipleChoice(){
     document.querySelector('#d').innerHTML = quiz_mc[q_counter - 1][3];
 }
 
+function recordAnswers(){
 
+}
+
+function checkAnswers(){
+    for(let i=0, k=0;i<user_answers.length,k<real_answers.length;i++,k++){
+        total_counter+=1;
+        if (i==k){
+            right_counter+=1
+        }
+        else{
+            wrong_counter+=1
+        }
+    }
+    console.log(total_counter, right_counter, wrong_counter)
+}
--- quizbank.js
+++ quizbank.js
@@ -50,89 +50,260 @@ const quiz_bank = {
                     }
                 },
         "world_geography":{
-            "easy":{
-                "q1":"What is the capitol of Spain?",
-                "q2":"What is the largest country by land mass?",
-                "q3":"What is the most populated country?"
-            },
-            "easy_a":{
-                "a1":"Madrid",
-                "a2":"Russia",
-                "a3":"India"
+            "levels":{
+                "easy":["What is the capitol of Spain?",
+                "What is the largest country by land mass?",
+                "What is the most populated country?",
+                "What country is NOT in North America?",
+                "What is the capitol of Mexico?"],
+
+                "easy_a":["Madrid","Russia","India","Belize","Mexico City"],
+
+                "easy_mc":[["Barcelona","Madrid","Valencia","Grenada"],
+                ["Russia","USA","China","India"],
+                ["Brazil","USA","China","India"],
+                ["Canada","USA","Mexico","Ecuador"],
+                ["Mexico City","Cancun","Playa del Carmen","Cabo"]],
+
+                "medium":["What is the most populated country in Western Europe?",
+                "What is the only Jewish majority country in the world?",
+                "What is the most populated country in Africa?",
+                "What country surrounds the Suez Canal?",
+                "What city straddles both Europe and Asia?"],
+
+                "medium_a":["Germany","Israel","Nigeria","Egypt","Istanbul"],
+
+                "medium_mc":[["France","Germany","Spain","Portugal"],
+                ["Eritrea","Rwanda","Israel","Palestine"],
+                ["Ethiopia", "South Africa","Egypt","Nigeria"],
+                ["Turkey","Qatar","Egypt","Morocco"],
+                ["Moscow","Athens","Istanbul","Kyiv"]],
+
+                "hard":["What is the smallest country in Europe by land mass?",
+                "What microstate is between France and Spain in the Pyrenes?",
+                "What country in Europe is named after a historically influential woman named 'Rosa'?",
+                "What is the smallest country in the world by land mass?",
+                "What is the name of the uninhabited land between Central and South America?"],
+
+                "hard_a":["Vatican City","Andorra","Luxemburg","Vatican City",""],
+
+                "hard_mc":[["Monaco","Vatican City","Croatia","Luxemburg"],
+                ["Andorra","Micronesia","Luxemburg","Judea"],
+                ["Bulgaria","Romania","Luxemburg","Andorra"],
+                ["Monaco","Vatican City","Croatia","Luxemburg"],
+                ["Ngev Desert","Panama Gap","Darien Gap","Andes Mountains"]]
+        }
+    },
+        "historical_geography":{
+            "levels":{
+                "easy":["What empire was the USA previously a colony of?",
+                "What empire was South Africa a colony of?",
+                "What ancient civilization built the Pyramids?",
+                "What was the 'super continent' called?",
+                "What European empire did Florida first belong to?"],
+
+                "easy_a":["Great Britain","Great Britain","Egyptians","Pangea","Spain"],
+                
+                "easy_mc":[["France","Great Britain","Spain","Portugal"],
+                ["France","Great Britain","Spain","Portugal"],
+                ["Greek","Roman","Egyptian","Gaelic"],
+                ["Pangea","Adastra","Geogea","Eurasia"],
+                ["France","Great Britain","Spain","Portugal"]],
+
+                "medium":["Which country is surrounded by South Africa?",
+                "What country was divided into East and West following WWII?",
+                "What city was divided into four sectors following WWII?",
+                "What country is the Alps not in?",
+                "Which language is spoken in Brazil?"],
+
+                "medium_a":["Lesotho","Germany","Berlin","Spain","Portuguese"],
+                
+                "medium_mc":[["Botswana","Lesotho","Zimbabwe","Namibia"],
+                ["Vietnam","France","Germany","Austria"],
+                ["Vienna","Budapest","Paris","Berlin"],
+                ["France","Italy","Austria","Spain"],
+                ["Spanish","English","Portuguese","Catalan"]],
+
+                "hard":["What historical kingdom was Barcelona in?",
+                "What country was not part of the Holy Roman Empire?",
+                "Which Arab invasion of Israel shaped its modern borders?",
+                "Which empire encompassed most of the Middle East before 1917?",
+                "What is the most recent new country?"],
+
+                "hard_a":["Catalonia","Denmark","1967 Arab Invasion","Ottoman","South Sudan"],
+
+                "hard_mc":[["Spain","Venice","Catalonia","Transjordan"],
+                ["Germany","Poland","Italy","Spain"],
+                ["1948 Independence War","Arab Invasion of 1967","1973 Yom Kippur War","Second Intifada 2000-05"],
+                ["Ottoman","Greek","Great Britain","Iranian"],
+                ["New Guinea","Myanmar","South Sudan","Niger"]]
+            }
+        },
+        "US_history":{
+            "levels":{
+                "easy":["Who was the first President?",
+                "Who was the president during the Civil War?",
+                "Which president served four terms?",
+                "Which state was not one of the original 13 colonies?",
+                "Who was the first person to make an American flag?"],
+
+                "easy_a":["George Washington","Abraham Lincoln","FDR","Florida","Betsy Ross"],
+
+                "easy_mc":[["George Washington","Thomas Jefferson","Sam Adams","James Madison"],
+                ["Abraham Lincoln","FDR","Ulysses S Grant","Harry Truman"],
+                ["TDR","JFK","FDR","Herbert Hoover"],
+                ["New York","Vermont","Georgia","Florida"],
+                ["Sybil Ludington","Betsy Ross","Molly Pither","Abigail Adams"]],
+
+                "medium":[],
+
+                "medium_a":[],
+
+                "medium_mc":[[],[],[],[],[]],
+
+                "hard":[],
+
+                "hard_a":[],
+
+                "hard_mc":[[],[],[],[],[]]
+            }
+        },
+        "world_history":{
+            "levels":{
+                "easy":[],
+                "easy_a":[],
+                "easy_mc":[[],[],[],[],[]],
+                "medium":["Which country was NOT one of the seven countries that invaded Israel in 1948?"],
+                "medium_a":[],
+                "medium_mc":[[],[],[],[],[]],
+                "hard":[],
+                "hard_a":[],
+                "hard_mc":[[],[],[],[],[]]
+            }
+        },
+        "US_politics":{
+            "levels":{
+                "easy":[],
+                "easy_a":[],
+                "easy_mc":[[],[],[],[],[]],
+                "medium":[],
+                "medium_a":[],
+                "medium_mc":[[],[],[],[],[]],
+                "hard":[],
+                "hard_a":[],
+                "hard_mc":[[],[],[],[],[]]
+            }
+        },
+        "world_politics":{
+            "levels":{
+                "easy":[],
+                "easy_a":[],
+                "easy_mc":[[],[],[],[],[]],
+                "medium":[],
+                "medium_a":[],
+                "medium_mc":[[],[],[],[],[]],
+                "hard":[],
+                "hard_a":[],
+                "hard_mc":[[],[],[],[],[]]
+            }
+        },
+        "economics":{
+            "levels":{
+                "easy":[],
+                "easy_a":[],
+                "easy_mc":[[],[],[],[],[]],
+                "medium":[],
+                "medium_a":[],
+                "medium_mc":[[],[],[],[],[]],
+                "hard":[],
+                "hard_a":[],
+                "hard_mc":[[],[],[],[],[]]
+            }
+        },
+        "finance":{
+            "levels":{
+                "easy":[],
+                "easy_a":[],
+                "easy_mc":[[],[],[],[],[]],
+                "medium":[],
+                "medium_a":[],
+                "medium_mc":[[],[],[],[],[]],
+                "hard":[],
+                "hard_a":[],
+                "hard_mc":[[],[],[],[],[]]
+            }
+        },
+        "philosopghy":{
+            "levels":{
+                "easy":[],
+                "easy_a":[],
+                "easy_mc":[[],[],[],[],[]],
+                "medium":[],
+                "medium_a":[],
+                "medium_mc":[[],[],[],[],[]],
+                "hard":[],
+                "hard_a":[],
+                "hard_mc":[[],[],[],[],[]]
+            }
+        },
+        "sports":{
+            "levels":{
+                "easy":[],
+                "easy_a":[],
+                "easy_mc":[[],[],[],[],[]],
+                "medium":[],
+                "medium_a":[],
+                "medium_mc":[[],[],[],[],[]],
+                "hard":[],
+                "hard_a":[],
+                "hard_mc":[[],[],[],[],[]]
+            }
+        },
+        "music":{
+            "levels":{
+                "easy":[],
+                "easy_a":[],
+                "easy_mc":[[],[],[],[],[]],
+                "medium":[],
+                "medium_a":[],
+                "medium_mc":[[],[],[],[],[]],
+                "hard":[],
+                "hard_a":[],
+                "hard_mc":[[],[],[],[],[]]
             }
         }
-    }
+}
 }
 
-    // "economics";{
-    //     "micro";{
-    
-    //     },
-    //     "macro";{
-    
-    //     }
-    
-    // },
-    // "world_history_questions":{
-    //     "people":{
-    
-    //     },
-    //     "places":{
-    
-    //     },
-    //     "events":{
-    
-    //     }
-    // },
-    // "US_politics":{
-    //     "people":{},
-    //     "policies":{},
-    //     "government":{},
-    //     "scandals":{}
-    // },
-    // "US_history":{
-    //     "people":{
-    
-    //     },
-    //     "places":{
-    
-    //     },
-    //     "events":{
-    
-    //     }
-    // },
-    // "world_politics":{
-    //     "people":{},
-    //     "policies":{},
-    //     "organizations":{}
-    // },
-    // "sports":{
+const quiz_list=[];
 
-    //     },
-    // "finance":{
+const temp_quiz_list=[];
 
-    //         }
-
-const quiz_list=[]
-
-const temp_quiz_list=[]
-
-const topic_list=["geography"]
-const geography_topic_list=["us_state_capitols","world_geography"]
-
-class quizzle {
-    constructor (quizTopic, quizSubTopic, quizLevel) {
-        this.quizTopic = quizTopic;
-        this.quizSubTopic = quizSubTopic;
-        this.quizLevel =  quizLevel;
-        this.quiz = (quizTopic, quizLevel)
-        this.element = null;
-    }
+let user_answers = [];
+let real_answers = [];
+let right_counter = 0;
+let wrong_counter = 0;
+let total_counter = 0;
+if (total_counter != 0){
+    let percentage = (right_counter/total_counter);
+}
+else {
+    let percentage = 0;
 }
 
-class question {
-    constructor(question,answer){
-        this.question= null;
-        this.answer= null;
-    }
-}
+// class quizzle {
+//     constructor (quizTopic, quizSubTopic, quizLevel) {
+//         this.quizTopic = quizTopic;
+//         this.quizSubTopic = quizSubTopic;
+//         this.quizLevel =  quizLevel;
+//         this.quiz = (quizTopic, quizLevel)
+//         this.element = null;
+//     }
+// }
+
+// class question {
+//     constructor(question,answer){
+//         this.question= null;
+//         this.answer= null;
+//     }
+// }
