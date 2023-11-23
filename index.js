2b8bb72d306760174b8dee7a6227931f11e06aaf
jsklar03 <jonah.sklar@gmail.com>
2023-11-22 21:07:18 -0500

    index page pulls from local storage

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
+// To get scoreboard elements from the DOM
 let total_score = document.querySelector('#total_q');
 let right_score = document.querySelector('#correct_score');
 let wrong_score = document.querySelector('#wrong_score');
 let percentage = document.querySelector('#percent_score');
 
+
+// to Update the scoreboard
 let update_right = localStorage.getItem('right_num') 
 let update_wrong = localStorage.getItem('wrong_num')
 let update_total = localStorage.getItem('stored_quizzes')
 console.log(update_right,update_wrong,update_total)
-console.log(typeof(update_right))
 
-function retrieveFromLocalStorage() {
-    // gets users answers
-    console.log(localStorage)
-    let saved_scores = localStorage.getItem('storedQuizzes');
-    console.log(saved_scores);
-    updateScoreboard()
-}
+let right_int = parseInt(update_right);
+let wrong_int = parseInt(update_wrong);
+let total_int = parseInt(update_total);
 
-function updateScoreboard(total_counter,right_counter){
-    let w_score = total_counter - right_counter;
-    let t_score = total_counter;
-    let r_score = right_counter;
-    console.log(w_score,t_score,r_score)
+total_score.innerHTML=total_int;
+right_score.innerHTML=right_int;
+wrong_score.innerHTML=wrong_int;
+console.log(typeof(total_int));
 
-}
+
+console.log(typeof(right_int,wrong_int,total_int))
+console.log(right_int,wrong_int,total_int)
+
+
 
 //also need to update the pie chart
