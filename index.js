0fec94ec92719ace1141e19d80eee7017f06777d
jsklar03 <jonah.sklar@gmail.com>
2023-11-22 23:27:53 -0500

    fixed home page

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // To get scoreboard elements from the DOM
-let total_score = document.querySelector('#total_q');
 let right_score = document.querySelector('#correct_score');
 let wrong_score = document.querySelector('#wrong_score');
 let percentage = document.querySelector('#percent_score');
@@ -13,11 +12,11 @@ console.log(update_right,update_wrong,update_total)
 
 let right_int = parseInt(update_right);
 let wrong_int = parseInt(update_wrong);
-let total_int = parseInt(update_total);
 
-total_score.innerHTML=total_int;
+
 right_score.innerHTML=right_int;
 wrong_score.innerHTML=wrong_int;
+percentage.innerHTML=(right_int/(right_int+wrong_int)*100)+"%"
 
 function executeD3 (right_int,wrong_int){
     let pieData = [
