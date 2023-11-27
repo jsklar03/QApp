f00d19ad2f39ad7bd1511574219c688e69aeee37
jsklar03 <jonah.sklar@gmail.com>
2023-11-27 12:58:58 -0500

    changes images in topics page

--- topics.js
+++ topics.js
@@ -1,5 +1,5 @@
-document.getElementById("easy").children[0].setAttribute("href", "../quiz.html?topic="+ topic + "&level=Easy");
-document.getElementById("medium").children[0].setAttribute("href", "../quiz.html?topic="+ topic + "&level=Medium");
-document.getElementById("hard").children[0].setAttribute("href", "../quiz.html?topic="+ topic + "&level=Hard");
+document.getElementById("easy_box").children[0].setAttribute("href", "../quiz.html?topic="+ topic + "&level=Easy");
+document.getElementById("medium_box").children[0].setAttribute("href", "../quiz.html?topic="+ topic + "&level=Medium");
+document.getElementById("hard_box").children[0].setAttribute("href", "../quiz.html?topic="+ topic + "&level=Hard");
 
 document.querySelector('.quiz_title').innerHTML = topic
