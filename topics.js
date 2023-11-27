7183ba890c24ec9ef465b18dd6d1e51c46d9e0d7
jsklar03 <jonah.sklar@gmail.com>
2023-11-27 07:41:06 -0500

    Added Topics JS update and img credits

--- topics.js
+++ topics.js
@@ -1,3 +1,5 @@
 document.getElementById("easy").children[0].setAttribute("href", "../quiz.html?topic="+ topic + "&level=Easy");
 document.getElementById("medium").children[0].setAttribute("href", "../quiz.html?topic="+ topic + "&level=Medium");
 document.getElementById("hard").children[0].setAttribute("href", "../quiz.html?topic="+ topic + "&level=Hard");
+
+document.querySelector('.quiz_title').innerHTML = topic
